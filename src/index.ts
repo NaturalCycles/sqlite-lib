3533b973a91a017bba6a0bc61a54ba46d768f22f
kirillgroshkov <kirill.groshkov@naturalcycles.com>
2023-12-28 17:48:55 +0100

    fix: deps

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-import { SQLiteDB, SQLiteDBCfg } from './sqlite.db'
+export * from './sqlite.db'
 export * from './sqliteKeyValueDB'
 export * from './betterSqliteKeyValueDB'
-
-export type { SQLiteDBCfg }
-
-export { SQLiteDB }
